d39049369976e88739b701380d2947c7fa90d1d8
Pedro Cotovio <pepedro97@gmail.com>
2020-04-23 22:30:07 +0100

    kaggle json

--- Storage/clean.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-gsutil rm -r gs://$GCLOUD_BUCKET
-
--- Storage/setup.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-gsutil mb -b on -l us-east1 gs://$GCLOUD_BUCKET/
-
-gsutil cp sdmanager_db.json gs://$GCLOUD_BUCKET/fixture_dirs
